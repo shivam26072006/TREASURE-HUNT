a866ff22cde0c3006467e809f4d2149b5aead12e
shivam26072006 <73473655+shivam26072006@users.noreply.github.com>
2021-06-04 22:13:48 +0530

    Add files via upload

---
+++ js/System.js
@@ -0,0 +1,30 @@
+class System{
+
+    constructor(){}
+
+    authenticate(actualCode,enteredCode){
+        textSize(50);
+        fill("black");
+        text(code,300,300)
+        
+
+    if(actualCode === enteredCode.toUpperCase())
+         return true
+    else
+         return fasle
+
+    if(score === 3) {
+        
+        clear()
+        background(bg2)
+        fill("black")
+        textSize(40);
+        text("TREASURE UNLOCKED",250,200);
+    
+        }
+
+        
+    }
+
+  
+}
